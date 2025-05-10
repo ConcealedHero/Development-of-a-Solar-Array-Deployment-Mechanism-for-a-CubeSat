e43143755b4d217334147ebbad7622e0f98a194e
Christopher Hernandez <31148977+ConcealedHero@users.noreply.github.com>
2025-05-09 18:13:48 -0700

    NEMA_11_Stepper_Motor.ino
    Signed-off-by: Christopher Hernandez <31148977+ConcealedHero@users.noreply.github.com>

---
+++ NEMA_11_Stepper_Motor.ino
@@ -0,0 +1,44 @@
+/*
+    Christopher Hernandez
+    Development of a Solar Array Deployment Mechanism for a CubeSat
+    NEMA 11 Integration Test
+    This program tests whether the NEMA 11 motor could rotate CW and CCW.
+*/
+
+const int stepPin = 10;
+const int dirPin = 9;
+const int stepsPerRevolution = 200;
+
+/*
+  Current draw testing
+           Step-dir pins
+  Motor 4: 8-7, 0.275 amps
+  Motor 3: 10-9, 0.340 amps
+  Motor 2: 6-5, 0.330 amps
+  Motor 1: 4-3, 0.355 amps
+*/
+
+void setup() {
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(dirPin, HIGH);
+  for(int x=0; x < 4 * stepsPerRevolution; x++){
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(1000);
+
+  digitalWrite(dirPin, LOW);
+  for(int x=0; x < 4 * stepsPerRevolution; x++){
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+  }
+  delay(1000);
+}
